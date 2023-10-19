82508ae8f1aacf4f1ddbe5536e7954c534a0f164
Antonio Quintanilla <antonioq1999@gmail.com>
2023-10-19 11:44:32 -0700

    Section 07 Modules added

---
+++ Section 3 Intro to Python/07 Modules/check_imports.py
@@ -0,0 +1,9 @@
+#  How to use import statements for accessing modules from different directories
+import sys
+
+sys.path.insert(1,r'C:\Users\Antonio\Desktop\MBDC-code\Section 3 Intro to Python\07 Modules')
+
+import trial
+
+print (trial.names)
+
