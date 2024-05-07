758a042bd0251bf4ff9f04faccb2bdfb83bf7d46
Antonio Quintanilla <antonioq1999@gmail.com>
2024-05-07 10:26:25 -0700

    Addition of practice projects

--- Section 5 Django Web Framework/djenv/demoproject/demoproject/settings.py
+++ Section 5 Django Web Framework/djenv/demoproject/demoproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$nrna+%ok@rb3!k7j8y4#bh79k%7n1gva4jhxb37-@dga#=av8'
+SECRET_KEY = 'django-insecure-3%m6*k@@q)al&z0$knb8&)*&rr^iibj3udex+$%+coozo2bd6f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'demoapp',
 ]
 
 MIDDLEWARE = [
